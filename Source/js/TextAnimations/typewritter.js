33133ed6d178c4c62b1d2f77b75e708fc02c6ff4
VolodymyrS <volodymyr.sh.05@gmail.com>
2024-10-19 00:13:08 +0300

    modified typeWritterEffect

--- Source/js/TextAnimations/typewritter.js
+++ Source/js/TextAnimations/typewritter.js
@@ -1,13 +1,13 @@
 export function typeWritterEffect(element, speed) {
-    let fullText = element.innerHTML;  // Отримуємо повний текст
-    element.innerHTML = '';            // Очищуємо елемент для початку анімації
-    let i = 0;                         // Індекс для виведення літер
+    let fullText = element.innerHTML;
+    element.innerHTML = '';
+    let i = 0;
 
     function showChar() {
-        if (i < fullText.length) {      // Перевіряємо змінну fullText, а не text
-            element.innerHTML += fullText[i];  // Додаємо наступну літеру
+        if (i < fullText.length) {
+            element.innerHTML += fullText[i];
             i++;
-            setTimeout(showChar, speed);       // Встановлюємо затримку
+            setTimeout(showChar, speed);
         }
     }
 
--- Source/js/script.js
+++ Source/js/script.js
@@ -3,9 +3,13 @@ import {typeWritterEffect} from './TextAnimations/typeWritter.js';
 document.addEventListener('DOMContentLoaded', () => {
     let nameTitleElement = document.getElementById('name-title');
     let devTitleElement = document.getElementById('developer-title');
+    devTitleElement.style.opacity = 0;
     let speed = 150; // Швидкість виведення літер
 
     typeWritterEffect(nameTitleElement, speed);
-    typeWritterEffect(devTitleElement, speed);
+    setTimeout(() => {
+        devTitleElement.style.opacity = 1;
+        typeWritterEffect(devTitleElement, speed)
+    }, 3000);
 })
 
